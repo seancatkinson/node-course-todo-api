e9c706578e38d37bf63dd85f4e403fccb3032dc6
Sean Atkinson <sean@dabbledabble.co>
2018-08-19 12:57:05 +0100

    Fix type in mongodb uri env path

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const dbPath = process.env.MONODB_URI || 'mongodb://localhost:27017/TodoApp';
+const dbPath = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect( dbPath);
 
 module.exports = {mongoose};
