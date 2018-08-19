a98e4dbca5fa5e7f3494bd3121919b6beb56ab5e
Sean Atkinson <sean@dabbledabble.co>
2018-08-19 14:20:40 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -73,6 +74,36 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (ObjectID.isValid(id) == false) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if (todo == null) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
